a96d93e076552b4d19c506e5a3d112771be6b456
Burhanuddin <buscbd@gmail.com>
2024-02-21 19:42:58 +0600

    added sum

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const allBtns = document.getElementsByClassName('add-btn');
 for (const btn of allBtns) {
     btn.addEventListener('click', function(e) {
-        const name = e.target.parentNode.childNodes[1].innerTex;
+        const name = e.target.parentNode.childNodes[1].innerText;
         const rate = e.target.parentNode.childNodes[3].childNodes[1].innerText;
         const category = e.target.parentNode.childNodes[5].childNodes[1].innerText;
 
@@ -17,9 +17,32 @@ for (const btn of allBtns) {
         tr.appendChild(td1);
         tr.appendChild(td2);
         tr.appendChild(td3);
-
         playerContainer.appendChild(tr);
 
-        // console.log(e.target.parentNode.childNodes[1].innerTex)
+
+        // Total cost 
+        calc('total-cost', rate);
+        calc('grand-cost', rate);
     });
+}
+
+// Cuopon code 
+const cuoponBtn = document.getElementById('apply-btn');
+cuoponBtn.addEventListener('click', function(e) {
+    const cuopon = e.target.parentNode.childNodes[1].value;
+    const totalCost = document.getElementById('total-cost');
+    const totalToInt = parseInt(totalCost.innerText);
+
+    if(cuopon == 'love24') {
+        // calc('grand-cost', totalToInt * 0.5);
+        console.log(totalToInt * .5 );
+    }
+});
+
+
+
+function calc(id, value) {
+    const totalCost = document.getElementById(id);
+    const totalToInt = parseInt(totalCost.innerText);
+    totalCost.innerText = totalToInt + parseInt(value);
 }
