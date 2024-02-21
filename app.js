6fbc1472e3da5767a69680ec434a8acf6c2cfae7
Burhanuddin <buscbd@gmail.com>
2024-02-21 22:30:04 +0600

    added count

--- app.js
+++ app.js
@@ -14,12 +14,26 @@ for (const btn of allBtns) {
         const td3 = document.createElement('td');
         td3.innerText = category;
 
+        const cartCountLimit = toInt('cart');
+        if(cartCountLimit + 1 > 6 ) {
+            alert("Limit crossed!");
+            return;
+        }
+
         tr.appendChild(td1);
         tr.appendChild(td2);
         tr.appendChild(td3);
         playerContainer.appendChild(tr);
 
+        // Update budget
+        // const budget = toInt('budget');
+        const budget = document.getElementById('budget').innerText;
+        document.getElementById.innerText = parseInt(budget) - parseInt(rate);
 
+        const cartCount = toInt('cart');
+        document.getElementById('cart').innerText = cartCount + 1;
+        const leftCount = toInt('left');
+        document.getElementById('left').innerText = leftCount -1;
         // Total cost 
       cost("total-cost", rate);
       grandCost();
@@ -27,9 +41,20 @@ for (const btn of allBtns) {
     });
 }
 
-function grandCost(){
+function grandCost(status){
     const totalCost = toInt("total-cost");
-    document.getElementById('grand-cost').innerText = totalCost;
+    if(status == undefined) {
+        document.getElementById('grand-cost').innerText = totalCost; 
+    } else {
+        const cuoponInput = document.getElementById('coupon-input').value;
+
+        if(cuoponInput == "feb24") {
+            const discounts = totalCost * 0.2;
+            document.getElementById('grand-cost').innerText = totalCost - discounts; 
+        } else {
+            alert("Please enter a valid cuopon!");
+        }
+    }
 }
 
 function cost(id, value){
